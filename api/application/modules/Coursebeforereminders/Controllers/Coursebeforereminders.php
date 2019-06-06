95462a7835b6766f36e18b830977c70b2a0c7283
Nikita <nikita.parmar@theopeneyes.in>
2019-06-06 18:58:40 +0530

    done

--- api/application/modules/Coursebeforereminders/Controllers/Coursebeforereminders.php
+++ api/application/modules/Coursebeforereminders/Controllers/Coursebeforereminders.php
@@ -15,22 +15,26 @@ class Coursebeforereminders extends CI_Controller
     public function insert_data()
     {
         $data = json_decode(trim(file_get_contents('php://input')), true);
-        $result = $this->Coursebeforereminders_model->insert($data);
+        if (!empty($data['CourseBeforeReminderId'])) {
+            $result = $this->Coursebeforereminders_model->update($data);
+        } else {
+            $result = $this->Coursebeforereminders_model->insert($data);
+        }
         if ($result) {
-            echo json_encode($data);
+            echo json_encode($result);
         }
     }
     public function course_list()
-	{
-		$data = $this->Coursebeforereminders_model->course_list();
-		$res = $data->result();
-		echo json_encode($res);
+    {
+        $data = $this->Coursebeforereminders_model->course_list();
+        $res = $data->result();
+        echo json_encode($res);
     }
-    
+
     public function getAllDetails()
-	{
-		$data = $this->Coursebeforereminders_model->getAllDetails();
-		$res = $data->result();
-		echo json_encode($res);
-	}
+    {
+        $data = $this->Coursebeforereminders_model->getAllDetails();
+        $res = $data->result();
+        echo json_encode($res);
+    }
 }
--- api/application/modules/Coursebeforereminders/Models/Coursebeforereminders_model.php
+++ api/application/modules/Coursebeforereminders/Models/Coursebeforereminders_model.php
@@ -2,49 +2,35 @@
 
 class Coursebeforereminders_model extends CI_Model
 {
-    // public function update($data)
-    // {
-    // 	if ($data) {
-    //         $post_data = $data;
-    // 		$updatetdata1 = array(
-    //             "ConfigurationId" => 17,
-    //             "key" => "coursebeforereminder1",
-    //             "value" => $post_data[0] ? trim($post_data[0]['day1']) : 0,
-    //             "Description" => 
-    //             ($post_data[0]['candidate'] ? $post_data[0]['candidate'] : '0')
-    //             . ',' .
-    //             ($post_data[0]['instructor'] ? $post_data[0]['instructor'] : '0')
-    //         );
-    //         $updatetdata2 = array(
-    //             "ConfigurationId" => 18,
-    //             "key" => "coursebeforereminder2",
-    //             "value" => trim($post_data[1]['day2']),
-    //             "Description" => 
-    //             ($post_data[1]['candidate'] ? $post_data[1]['candidate'] : '0')
-    //             . ',' .
-    //             ($post_data[1]['instructor'] ? $post_data[1]['instructor'] : '0')
-    //         );
-    //         $updatetdata3 = array(
-    //             "ConfigurationId" => 19,
-    //             "key" => "coursebeforereminder3",
-    //             "value" => trim($post_data[2]['day3']),
-    //             "Description" => 
-    //             ($post_data[2]['candidate'] ? $post_data[2]['candidate'] : '0')
-    //             . ',' .
-    //             ($post_data[2]['instructor'] ? $post_data[2]['instructor'] : '0')
-    //         );
-    //        // print_r($updatetdata);
-    //         $updatetdata = array($updatetdata1,$updatetdata2,$updatetdata3);
-    // 		$res = $this->db->update_batch('tblmstconfiguration', $updatetdata, 'ConfigurationId');
-    // 		if ($res) {
-    // 			return true;
-    // 		} else {
-    // 			return false;
-    // 		}
-    // 	} else {
-    // 		return false;
-    // 	}
-    // }
+    public function update($data)
+    {
+    	if ($data) {
+            $post_data = $data;
+            
+            $updatetdata = array(
+                "CourseId" => trim($post_data['CourseId']),
+                "RemainderDay1" => trim($post_data['day1']),
+                "RemainderDay2" => trim($post_data['day2']),
+                "RemainderDay3" => trim($post_data['day3']),
+                "Reminder1SendTo" => trim(($post_data['candidate1'] ? $post_data['candidate1'] : '0')
+                    . ',' . ($post_data['instructor1'] ? $post_data['instructor1'] : '0')),
+                "Reminder2SendTo" => trim(($post_data['candidate2'] ? $post_data['candidate2'] : '0')
+                    . ',' . ($post_data['instructor2'] ? $post_data['instructor2'] : '0')),
+                "Reminder3SendTo" => trim(($post_data['candidate3'] ? $post_data['candidate3'] : '0')
+                    . ',' . ($post_data['instructor3'] ? $post_data['instructor3'] : '0')),
+            );
+         
+            $this->db->where('CourseBeforeReminderId', $post_data['CourseBeforeReminderId']);
+            $res = $this->db->update('tblcoursebeforereminder', $updatetdata);
+    		if ($res) {
+    			return true;
+    		} else {
+    			return false;
+    		}
+    	} else {
+    		return false;
+    	}
+    }
 
     public function insert($data)
     {
