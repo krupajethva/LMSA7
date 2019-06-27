f1ac9ce96d3eebdb19419e0b0ea8ce36941b0323
Nikita <nikita.parmar@theopeneyes.in>
2019-06-27 11:24:12 +0530

    s

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -27,65 +27,41 @@ import { DatePipePipe } from './datepipe/date-pipe.pipe';
 
 import { LoginlogComponent } from './loginlog/loginlog.component';
 import { EmaillogComponent } from './emaillog/emaillog.component';
-
-
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './services/user.service';
-
-
 import { UserinstructorlistComponent } from './userinstructorlist/userinstructorlist.component';
-
 import { OpeninstructorComponent } from './openinstructor/openinstructor.component';
-
 import { CountryComponent } from './country/country.component';
 import { CountrylistComponent } from './countrylist/countrylist.component';
-
 import { StateComponent } from './state/state.component';
 import { StatelistComponent } from './statelist/statelist.component';
-
 import { EducationComponent } from './education/education.component';
 import { EducationlistComponent } from './educationlist/educationlist.component';
-
 import { EmailtemplateComponent } from './emailtemplate/emailtemplate.component';
 import { EmailtemplateListComponent } from './emailtemplate-list/emailtemplate-list.component';
 import { EmailtemplateService } from './services/emailtemplate.service';
-
 import { SettingsComponent } from './settings/settings.component';
-
 import { DashboardLearnerComponent } from './dashboard-learner/dashboard-learner.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
 import { InviteInstructorComponent } from './invite-instructor/invite-instructor.component';
-
-
-
 import { RegisterInstructorInvitedComponent } from './register-instructor-invited/register-instructor-invited.component';
-
-
 import { AdminListComponent } from './admin-list/admin-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
 import { CertificateComponent } from './certificate/certificate.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
-
-
 import { RegisterLearnerComponent } from './register-learner/register-learner.component';
 import { UserActivationComponent } from './user-activation/user-activation.component';
-
 import { RegisterAdminComponent } from './register-admin/register-admin.component';
-
 import { EditProfileAdminComponent } from './edit-profile-admin/edit-profile-admin.component';
 import { EditProfileService } from './services/edit-profile.service';
 import { LinkListComponent } from './link-list/link-list.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
-
 import { HeaderRegisterComponent } from './header-register/header-register.component';
 import { FooterRegisterComponent } from './footer-register/footer-register.component';
-
 import { CategoryComponent } from './category/category.component';
 import { CategorylistComponent } from './categorylist/categorylist.component';
 import { CourseComponent } from './course/course.component';
@@ -98,10 +74,7 @@ import { CourseListComponent } from './course-list/course-list.component';
 import { LearnerCoursesComponent } from './learner-courses/learner-courses.component';
 import { InstructorfollowersComponent } from './instructorfollowers/instructorfollowers.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-
 import { TagInputModule } from 'ngx-chips';
-
-
 import { InstructorCoursesComponent } from './instructor-courses/instructor-courses.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { AssessmenttestComponent } from './assessmenttest/assessmenttest.component';
