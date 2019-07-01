c150d29c39d9358046dc885594641cb78a645a52
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-07-01 02:19:26 -0400

    AC

--- logs/log-2019-07-01.php
+++ logs/log-2019-07-01.php
@@ -36,3 +36,15 @@ JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
 WHERE `tci`.`UserId` = '297'
 GROUP BY `tcs`.`SessionStatus`
 ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 11:42:11 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '297'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 11:42:35 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '297'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
