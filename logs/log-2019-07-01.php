32309285024f9ad8cc4885f85cf7a49bcb82fe72
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-07-01 02:09:55 -0400

    AC

---
+++ logs/log-2019-07-01.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-01 10:22:36 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '484'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 10:23:32 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '484'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 10:23:44 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '484'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 10:26:45 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '297'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 10:28:04 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '297'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
+ERROR - 2019-07-01 10:30:26 --> Query error: Expression #1 of ORDER BY clause is not in GROUP BY clause and contains nonaggregated column 'lms.tci.CourseSessionId' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by - Invalid query: SELECT count(tci.CourseSessionId) as courseview, `tcs`.`SessionStatus`
+FROM `tblcourseinstructor` as `tci`
+JOIN `tblcoursesession` `tcs` ON `tci`.`CourseSessionId`=`tcs`.`CourseSessionId`
+WHERE `tci`.`UserId` = '297'
+GROUP BY `tcs`.`SessionStatus`
+ORDER BY `tci`.`CourseSessionId` DESC
