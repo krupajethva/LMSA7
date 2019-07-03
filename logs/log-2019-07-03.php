7fc90c8e185f751c1386a864028a4b6bf04a8169
OPENEYES\Developer2 <nirav.patell@theopeneyes.in>
2019-07-03 16:29:59 +0530

    s

---
+++ logs/log-2019-07-03.php
@@ -0,0 +1,1 @@
+ERROR - 2019-07-03 16:10:31 --> mysqli_next_result(): There is no next result set. Please, call mysqli_more_results()/mysqli::more_results() to check whether to call this function/method at line no 732 in C:\wamp64\www\LMSA7\api\application\modules\CourseScheduler\models\CourseScheduler_model.php
