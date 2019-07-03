5923851062482c39cd26efab6470fff9157b7b88
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-07-03 02:33:18 -0400

    AC

---
+++ logs/log-2019-07-03.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-03 11:14:21 --> mysqli_next_result(): There is no next result set. Please, call mysqli_more_results()/mysqli::more_results() to check whether to call this function/method at line no 730 in C:\wamp64\www\LMSA7\api\application\modules\CourseScheduler\models\CourseScheduler_model.php
+ERROR - 2019-07-03 11:14:41 --> mysqli_next_result(): There is no next result set. Please, call mysqli_more_results()/mysqli::more_results() to check whether to call this function/method at line no 730 in C:\wamp64\www\LMSA7\api\application\modules\CourseScheduler\models\CourseScheduler_model.php
