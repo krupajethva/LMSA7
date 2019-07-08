beef4d875f15aa4b4b51dc7ea3b270408eaf41bf
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-07-08 00:00:17 -0400

    AC

---
+++ logs/log-2019-07-05.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-05 15:11:08 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-05 15:25:53 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
