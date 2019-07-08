12038907364d609848e8d4abec74301f480f8a9f
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-07-08 09:31:38 -0400

    AC

---
+++ logs/log-2019-07-08.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-08 16:44:23 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-08 16:44:28 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-08 16:52:53 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-08 16:54:16 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-08 17:08:25 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
+ERROR - 2019-07-08 17:25:50 --> Undefined variable: row1 at line no 28 in C:\wamp64\www\LMSA7\api\application\modules\Inbox\models\Inbox_model.php
