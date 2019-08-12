076e817896575a1744f801a28f739f7ea51e5d62
anandyadav380 <42231046+anandyadav380@users.noreply.github.com>
2019-08-12 02:26:48 -0400

    AC

--- src/app/admin/compose/compose.component.ts
+++ src/app/admin/compose/compose.component.ts
@@ -23,8 +23,10 @@ export class ComposeComponent implements OnInit {
       resize_minHeight: '100',
       resize_minWidth: '948',
       extraAllowedContent: 'span;ul;li;table;td;style;*[id];*(*);*{*}',
-      enterMode: Number(2)
+      enterMode: Number(2),
       //extraAllowedContent: 'style;*[id,rel](*){*}
+      //removePlugins : 'elementspath,save,image,flash,iframe,link,smiley,tabletools,find,pagebreak,templates,about,maximize,showblocks,newpage,language',
+      //removeButtons : 'Copy,Cut,Paste,Undo,Redo,Print,Form,TextField,Textarea,Button,SelectAll,NumberedList,BulletedList,CreateDiv,Table,PasteText,PasteFromWord,Select,HiddenField'
     });
 
   }
